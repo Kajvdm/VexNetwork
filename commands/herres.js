da69c4cae8913e95077dc9308560a2bc235968ac
Kajvdm <66601386+Kajvdm@users.noreply.github.com>
2020-06-08 11:42:56 +0200

    Create herres.js

---
+++ commands/herres.js
@@ -0,0 +1,11 @@
+const discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) =>{
+
+    return message.channel.send('https://www.youtube.com/watch?v=c1oJYVNyHrs');
+
+}
+
+module.exports.help = {
+    name: "herres"
+}
